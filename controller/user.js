59410a8ea87b04ef57dea9f73f8452c925f45f9c
Tracy.yang <tracy.yang@movit-tech.com>
2019-01-10 17:42:17 +0800

    修改sql

--- controller/user.js
+++ controller/user.js
@@ -4,23 +4,22 @@ let {success,falied} = require('../util/sendFormat');
 userMethods = {
     getUser:async(userName) => {
        await query('select * from users where userName = ?',userName)
-     },
-     login:async(ctx,next) =>{
+    },
+    login:async(ctx,next) =>{
         let userName = ctx.request.body.userName;
         let password = ctx.request.body.password
         let res;
-        await query('select * from users where userName = ? and password = ?',[userName,password]).then(undefined,data=>{
+        await query('select id,userName,headerImg from users where userName = ? and password = ?',[userName,password]).then(undefined,data=>{
             let userInfo = data[0];
             if(userInfo){
-                res = success('','登陆成功！')
+                res = success(userInfo,'登陆成功！')
             }else{
                 res = falied(403,'登陆失败！')
             }
         });
         console.log(res,3333);
         ctx.body = res;
-       
-     }
+    }
 }
 
 module.exports = userMethods;
